dd39fdc9bf890227bb334df77e4c1bfa73b71ccc
Zoe Wolter <zgwolter@gmail.com>
2017-04-17 23:43:58 -0500

    add percentages and more categories

--- graph.js
+++ graph.js
@@ -7,16 +7,33 @@ function makeGraph(data,category) {
 		displayNames['cap-color'] = {n:'Brown',b:'Buff',c:'Cinnamon',g:'Gray',r:'Green',p:'Pink',u:'Purple',e:'Red',w:'White', y:'Yellow'};
 		displayNames['bruises'] = {t: "Yes", f: "No"};
 		displayNames['odor'] = {a:'Almond',l:'Anise',c:'Creosote',y:'Fishy',f:'Foul',m:'Musty',n:'None',p:'Pungent',s:'Spicy'}
-	var width = 1200;
+		displayNames['gill-attachment'] = {a:'Attached',d:"Descending",f:"Free",n:"Notched"};
+		displayNames['gill-spacing'] = {c:"Close",w:"Crowded",d:"Distant"};
+		displayNames['gill-size'] = {b:"Broad", n: "Narrow"};
+		displayNames['gill-color'] = {k:'Black',n:'Brown',b:'Buff',h:"Chocolate",g:"Gray",r:"Green",o:"Orange",p:"Pink",u:"Purple",e:"Red",w:"White",y:"Yellow"};
+		displayNames['stalk-shape'] = {e:"Enlarging", t:"Tapering"};
+		displayNames['stalk-root'] = {b:"Bulbous",c:"Club",u:"Cup",e:"Equal",z:"Rhizomorphs",r:"Rooted",'?':"Missing"};
+		displayNames['stalk-surface-above-ring'] = {f:"Fibrous",y:"Scaly",k:"Silky",s:"Smooth"};
+		displayNames['stalk-surface-below-ring'] = {f:"Fibrous",y:"Scaly",k:"Silky",s:"Smooth"};
+		displayNames['stalk-color-above-ring'] = {n:"Brown",b:"Buff",c:"Cinnamon",g:"Gray",o:"Orange",p:"Pink",e:"Red",w:"White",y:"Yellow"};
+		displayNames['stalk-color-below-ring'] = {n:"Brown",b:"Buff",c:"Cinnamon",g:"Gray",o:"Orange",p:"Pink",e:"Red",w:"White",y:"Yellow"};
+		displayNames['veil-type'] = {p:"Partial",u:"Universal"};
+		displayNames['veil-color'] = {n:"Brown",o:"Orange",w:"White",y:"Yellow"};
+		displayNames['ring-number'] = {n:"None",o:"One",t:"Two"};
+		displayNames['ring-type'] = {c:"Cobwebby",e:"Evanescent",f:"Flaring",l:"Large",n:"None",p:"Pendant",s:"Sheathing",z:"Zone"};
+		displayNames['spore-print-color'] = {k:'Black',n:'Brown',b:'Buff',h:"Chocolate",r:"Green",o:"Orange",u:"Purple",w:"White",y:"Yellow"};
+		displayNames['population'] = {a:"Abundant",c:"Clustered",n:"Numerous",s:"Scattered",v:"Several",y:"Solitary"};
+		displayNames['habitat'] = {g:"Grasses",l:"Leaves",m:"Meadows",p:"Paths",u:"Urban",w:"Waste",d:"Woods"};
+
+
+	var width = 1400;
 
 	var margin = {top: 20, bottom: 0, left: 10, right: 10};
 
-	var dotRadius = 5;
+	var dotRadius = 6;
 	var dotPadding = 2;
 	var columnMargin = 20;
 	var categories = Object.keys(displayNames[category]).map(function(key) { return key})
-	
-	console.log(noOfCirclesInARow);
 
 	var sumOfEveryGroup = {};
 	for(var i=0; i<data.length;i++) {
@@ -33,7 +50,6 @@ function makeGraph(data,category) {
 			sumOfEveryGroup[data[i][category]]['poisonous']++;
 		}
 	}
-
 	var xScale = d3.scalePoint()
 		.domain(categories)
 		.range([margin.left,width-margin.right])
@@ -41,6 +57,7 @@ function makeGraph(data,category) {
 		.padding(columnMargin);
 
 	var noOfCirclesInARow = Math.floor(xScale.step() / (dotRadius*2 + dotPadding*2));
+
 	var maxNoOfLinesInGroup = 0;
 	for(var group in sumOfEveryGroup) {
 		if(sumOfEveryGroup[group]['total']/noOfCirclesInARow > maxNoOfLinesInGroup) {
@@ -54,7 +71,7 @@ function makeGraph(data,category) {
 	var svg = d3.select('#mushroomChart')
 				.append('svg')
 				.attr("width", width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom + 25)
+				.attr("height", height + margin.top + margin.bottom + 45)
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var tallyOfGroupsX = {}
@@ -75,7 +92,7 @@ function makeGraph(data,category) {
 		}
 		var row = Math.floor(tallyOfGroupsY[group] / noOfCirclesInARow);
 		tallyOfGroupsY[group]++;
-		return 25+dotRadius + row * (dotRadius*2 +dotPadding);
+		return 45+dotRadius + row * (dotRadius*2 +dotPadding);
 	}
 
 	svg.append("g")
@@ -87,7 +104,7 @@ function makeGraph(data,category) {
 			.attr("cy", function(d,i) { return yRow(d); })
 			.attr("r", dotRadius)
 			.attr("fill", function(d,i) {
-				if(i<sumOfEveryGroup[d[category]]['poisonous']) {
+				if(d['class'] == 'p') {
 					return "#D60A0A";
 				} else {
 					return "#159002";
@@ -100,7 +117,22 @@ function makeGraph(data,category) {
 		.append("text")
 			.text( function(d) { return displayNames[category][d]; })
 			.attr("x", function(d) {return xScale(d)})
-			.attr("y", 20);
+			.attr("y", 20)
+	svg.append("g")
+		.selectAll("text")
+		.data(categories)
+		.enter()
+		.append("text")
+			.text (function(d) {
+				if (sumOfEveryGroup[d] != null) {
+					var poisonous = Math.round((sumOfEveryGroup[d]['poisonous']/sumOfEveryGroup[d]['total'])*100);
+				} else {
+					var poisonous = 0;
+				}
+				return poisonous +"% poison";
+			})
+			.attr("x", function(d) {return xScale(d)})
+			.attr("y", 40)
 return svg;
 
 }
